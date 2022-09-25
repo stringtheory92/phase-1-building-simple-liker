b5a0aad6dd52feb0099f0accee9f6fdecd08457e
Adam Kabak <adamkabak@yahoo.com>
2022-09-25 08:27:06 -0400

    commit tree 243e5aa5a3daf8fe55da3d9243d83765fd0d7f8b

--- main.js
+++ main.js
@@ -3,6 +3,41 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const likeGlyph = document.querySelector('.like-glyph')
+const errorField = document.querySelector('#modal')
+const errorMessage = document.querySelector('#modal-message')
+// EMPTY_HEART.className = 'deactivated-heart'
+//FULL_HEART.className = 'activated-heart'
+
+const configObj = body => {
+  return {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+      'accept': 'application/json'
+    },
+    body: JSON.stringify(body)
+  }
+}
+const heartSwitch = () => {
+  if (likeGlyph.className === 'activated-heart') {
+    likeGlyph.classList.remove('activated-heart');
+    likeGlyph.textContent = EMPTY_HEART;
+  } else {
+    likeGlyph.className = 'activated-heart';
+    likeGlyph.textContent = FULL_HEART;
+  }
+}
+const handleGlyphClick = e => {
+  mimicServerCall()
+  .then(heartSwitch)
+  .catch(e => {
+    errorField.className = '';
+    errorMessage.textContent = e;
+    setTimeout(() => errorField.className = 'hidden', 3000)
+  })
+}
+likeGlyph.addEventListener('click', handleGlyphClick)
 
 
 
